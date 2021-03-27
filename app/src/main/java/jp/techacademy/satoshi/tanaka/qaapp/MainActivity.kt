e538ebf37ffe941c24c36ea933bca3d4c0c0b84b
satosi1115jp <m13047st@g.edogawa-u.ac.jp>
2021-03-27 23:06:51 +0900

    チャプター5.5まで

--- app/src/main/java/jp/techacademy/satoshi/tanaka/qaapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/satoshi/tanaka/qaapp/MainActivity.kt
@@ -2,16 +2,19 @@ package jp.techacademy.satoshi.tanaka.qaapp
 
 import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 //findViewByID()を呼び出さずに該当Viewを取得するインポート宣言↓
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private var mGenre = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +31,13 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         }
+        //ナビゲーションドロワーの設定
+        val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar,R.string.app_name, R.string.app_name)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+
 
     }
 
@@ -46,4 +56,25 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val id=item.itemId
+
+        if (id==R.id.nav_hobby){
+            toolbar.title=getString(R.string.menu_hobby_label)
+            mGenre=1
+        }else if(id==R.id.nav_life){
+            toolbar.title=getString(R.string.menu_life_label)
+            mGenre=2
+        }else if (id==R.id.nav_health){
+            toolbar.title=getString(R.string.menu_health_label)
+            mGenre=3
+        }else if (id==R.id.nav_computer){
+            toolbar.title=getString(R.string.menu_computer_label)
+            mGenre=4
+        }
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
 }
+
